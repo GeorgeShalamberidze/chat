d3697ec87511a26b7da38949058961bfc3ed59d7
GIORGISHALAMBER\G.Shalamberidze <giorgishalamberidze1995@gmail.com>
2024-06-26 17:44:45 +0400

    ss

--- server/api/index.ts
+++ server/api/index.ts
@@ -23,14 +23,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: ["*"],
-    methods: ["GET", "POST"],
     credentials: true,
+    methods: ["GET", "POST"],
   })
 );
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 
   next();
 });
