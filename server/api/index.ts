5eae6e6fe85948e26bc7932301dabe7d205f31e4
GIORGISHALAMBER\G.Shalamberidze <giorgishalamberidze1995@gmail.com>
2024-06-26 17:37:16 +0400

    asds

--- server/api/index.ts
+++ server/api/index.ts
@@ -28,15 +28,9 @@ app.use(
   })
 );
 
-app.options("*", cors());
-
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
 
   next();
 });
